52d0d37b43258e2b0c55a7ab659d2af969653312
Arthur Young <arthur@Arthurs-MacBook-Pro.local>
2024-05-07 01:48:39 -0400

    included stat

--- tests/generate_data_demo.cc
+++ tests/generate_data_demo.cc
@@ -1,6 +1,7 @@
 #include "LD_framework.hh"
 #include "LD_ode_system.hh"
 #include "LD_integrators.hh"
+#include <sys/stat.h>
 
 #ifdef _OPENMP
   #include "omp.h"
@@ -37,7 +38,7 @@ char name_buffer[200];
 int main()
 {
   mkdir(dir_name, S_IRWXU);
-  
+
   generated_ode_observations inputs_gen(ode,nc,np);
   inputs_gen.set_random_ICs(LD_rng(9365),ode.get_default_IC_range());  // shay's number
   inputs_gen.generate_solution_curves(ode_integrator, ode.get_default_IC_indep_range());
